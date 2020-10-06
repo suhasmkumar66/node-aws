bb6b1a784b06db2bc1b58d6ada1cd99f1ff4c18d
Suhas M <suhasmkumar66@gmail.com>
2020-10-06 18:07:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res) => {
+    res.send("Welcome to first node js app");
+});
+app.listen(3000, () => {
+ console.log("suhas here");
+});
+

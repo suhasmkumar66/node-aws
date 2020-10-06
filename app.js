ca63ffb9f2a110f7c929affeecaec8da3baf1d1e
Suhas M <suhasmkumar66@gmail.com>
2020-10-06 18:22:49 +0530

    port added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 app.get('/',(req,res) => {
     res.send("Welcome to first node js app");
 });
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
  console.log("suhas here");
 });
 
